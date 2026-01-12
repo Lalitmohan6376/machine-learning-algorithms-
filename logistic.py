c0c304990780ab1fd52d86ddd99d7905b8a2383f
Lalit_mohan <lalitg6376@gmail.com>
2026-01-12 16:46:02 +0530

    Add files via upload

---
+++ logistic.py
@@ -0,0 +1,15 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# Data (0=fail,1=pass)
+X = [[1],[2],[3],[4],[5]]
+y = [0,0,0,1,1]
+
+#split the data into train and test
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=42)
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+pre = model.predict([[2.22]])
+print(pre)
