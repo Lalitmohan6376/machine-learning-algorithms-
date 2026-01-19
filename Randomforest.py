bfd8aebc4868e2d534492758fb90af669f4cfdfc
Lalit_mohan <lalitg6376@gmail.com>
2026-01-19 08:03:56 +0530

    Add files via upload

---
+++ Randomforest.py
@@ -0,0 +1,13 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+X = [[1,8],[2,7],[3,6],[4,6],[5,5],[6,5],[7,4],[8,3]]
+y = [0,0,0,1,1,1,1,1]
+
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=42)
+model = RandomForestClassifier()
+
+model.fit(X_train,y_train)
+pre = model.predict([[1,10]])
+print(pre[0])
