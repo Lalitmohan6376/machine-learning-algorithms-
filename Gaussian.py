d2a38d2a3a5c4bd81b585133ec8860d01ebf3127
Lalit_mohan <lalitg6376@gmail.com>
2026-01-30 19:08:59 +0530

    Update Gaussian.py

--- Gaussian.py
+++ Gaussian.py
@@ -1,6 +1,8 @@
 from sklearn.mixture import GaussianMixture
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Dataset
 X = np.array([
     [1, 2],
     [1, 4],
@@ -10,7 +12,15 @@ X = np.array([
     [10, 0]
 ])
 
+# Gaussian Mixture Model
 gmm = GaussianMixture(n_components=2, random_state=42)
 labels = gmm.fit_predict(X)
 
-print(labels)
+# Plot clusters
+plt.scatter(X[:, 0], X[:, 1], c=labels)
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.title("Gaussian Mixture Model Clustering")
+plt.show()
+
+print("Cluster Labels:", labels)
