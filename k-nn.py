fb44dc1427d2a2a3026d6690b154c7b1cc8556f4
Lalit_mohan <lalitg6376@gmail.com>
2026-02-02 20:29:26 +0530

    Update k-nn.py

--- k-nn.py
+++ k-nn.py
@@ -1,5 +1,6 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 X = [[40,20],[50,50],[60,90],[10,25],[70,70]]
@@ -17,4 +18,5 @@ result = fun()
 if result == 0:
     print("Blue")
 else:
+
     print("Red")
